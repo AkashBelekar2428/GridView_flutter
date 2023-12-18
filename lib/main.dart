f19b877feddaa796c34dd07c7d553527d8347ad3
Akash Belekar <112610706+AkashBelekar2428@users.noreply.github.com>
2023-12-18 10:37:07 +0530

    GridView with GridView.builder properties

--- lib/main.dart
+++ lib/main.dart
@@ -21,31 +21,77 @@ class gridViewScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
 
-    final colorArr = [Colors.grey,
+    final colorArr = [
+      Colors.grey,
       Colors.blueGrey,
       Colors.cyan,
       Colors.cyanAccent,
       Colors.pink,
       Colors.purple,
       Colors.orange,
-      Colors.green];
+      Colors.green
+    ];
 
     return Scaffold(
       appBar: AppBar(
         title: Text('GridView'),
       ),
-      body: GridView.count(crossAxisCount: 4,
-      crossAxisSpacing: 11,
-      mainAxisSpacing: 11,
-      children: [
-        Container(color: colorArr[0],),
-        Container(color: colorArr[1],),
-        Container(color: colorArr[2],),
-        Container(color: colorArr[3],),
-        Container(color: colorArr[4],),
-        Container(color: colorArr[5],)
-      ],
-      )
-    );
+       body: GridView.builder(itemBuilder: (context, index) {
+         return Container(color: colorArr[index],);
+
+       },itemCount: colorArr.length, gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+
+       crossAxisSpacing: 11,
+       mainAxisSpacing: 11,
+           maxCrossAxisExtent: 100
+       ),
+
+       )
+
+       );
+    //Column(
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   children: [
+      //     // Container(
+      //     //   height: 400,
+      //     //   child: GridView.count(crossAxisCount: 5 ,
+      //     //   crossAxisSpacing: 11,
+      //     //   mainAxisSpacing: 11,
+      //     //   children: [
+      //     //     Container(color: colorArr[0],),
+      //     //     Container(color: colorArr[1],),
+      //     //     Container(color: colorArr[2],),
+      //     //     Container(color: colorArr[3],),
+      //     //     Container(color: colorArr[4],),
+      //     //     Container(color: colorArr[5],),
+      //     //     Container(color: colorArr[6],),
+      //     //     Container(color: colorArr[7],),
+      //     //   ],
+      //     //
+      //     //   ),
+      //     // ),
+      //     Container(
+      //       height: 50,
+      //     ),
+      //     Container(
+      //      height: 400,
+      //       child: GridView.extent(maxCrossAxisExtent: 100,
+      //         crossAxisSpacing: 11,
+      //         mainAxisSpacing: 11,
+      //         children: [
+      //           Container(color: colorArr[0],),
+      //           Container(color: colorArr[1],),
+      //           Container(color: colorArr[2],),
+      //           Container(color: colorArr[3],),
+      //           Container(color: colorArr[4],),
+      //           Container(color: colorArr[5],),
+      //           Container(color: colorArr[6],),
+      //           Container(color: colorArr[7],)
+      //         ],
+      //       ),
+      //     )
+      //   ],
+      // )
+    // );
   }
 }
